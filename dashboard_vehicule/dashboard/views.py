f9cb20bc5f49878d0e98330605492149fff359c1
CedricLt44 <cedric.letutour@gmail.com>
2025-05-14 16:30:44 +0200

    maj du front dashboard et EDA

--- dashboard_vehicule/dashboard/views.py
+++ dashboard_vehicule/dashboard/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from .models_legacy import RawEntretiens
-# Create your views here.
+from django.db.models import Sum
+
+import locale
+# locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')  # Pour Linux/Mac
+locale.setlocale(locale.LC_ALL, '')  # Windows utilise la locale système
+
 def data_view(request):
     """
-    vue de mes donnée de la table postgres
-    via models_legacy.py.
+    Vue de mes données de la table PostgreSQL via models_legacy.py.
+    Permet de filtrer les données selon l'immatriculation et le type d'opération.
+    Affiche les immatriculations distinctes et les opérations distinctes.
     """
-  #  affichez la page de données
+    # Récupération de toutes les données
     entretien_data = RawEntretiens.objects.all()
 
-    return render(request, 'dashboard/data.html', {'entretien_data': entretien_data})
+    # Filtrer selon l'immatriculation si un paramètre 'immat' est présent dans l'URL
+    immat = request.GET.get('immat')
+    if immat:
+        entretien_data = entretien_data.filter(immat=immat)
+
+    # Extraction des immatriculations distinctes
+    distinct_immat = RawEntretiens.objects.values_list('immat', flat=True).distinct()
+
+    # Filtrer selon le type d'operation
+    operation = request.GET.get('operation')
+    if operation:
+        entretien_data = entretien_data.filter(operation=operation)
+    
+    # Opérations distinctes (tri avec None après le vide)
+    distinct_operation = list(
+        RawEntretiens.objects.values_list('operation', flat=True).distinct()
+    )
+    distinct_operation.sort(key=lambda x: (x is not None, x))
+
+    # Calcul du montant total des coûts
+    total_cost = entretien_data.aggregate(Sum('cost'))['cost__sum'] or 0
+    # Format avec espace comme séparateur de milliers et deux chiffres après la virgule
+    formatted_total_cost = f"{total_cost:,.2f}".replace(',', ' ').replace('.', ',')
+
+    return render(request, 'dashboard/data.html', {
+        'entretien_data': entretien_data,
+        'distinct_immat': distinct_immat,  # Passer les immatriculations distinctes au template
+        'distinct_operations': distinct_operation,  # Passer les opérations distinctes au template
+        'total_cost': formatted_total_cost,  # Passer le coût total au template
+    })
