f03e60c1584ae5a5fb11f3037714b6cc4686c330
CedricLt44 <cedric.letutour@gmail.com>
2025-05-14 20:33:52 +0200

    mep des charts et front

--- dashboard_vehicule/dashboard/views.py
+++ dashboard_vehicule/dashboard/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from .models_legacy import RawEntretiens
 from django.db.models import Sum
-
-import locale
-# locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')  # Pour Linux/Mac
-locale.setlocale(locale.LC_ALL, '')  # Windows utilise la locale système
+import plotly.express as px
+from django.utils.safestring import mark_safe
 
 def data_view(request):
     """
@@ -39,9 +37,48 @@ def data_view(request):
     # Format avec espace comme séparateur de milliers et deux chiffres après la virgule
     formatted_total_cost = f"{total_cost:,.2f}".replace(',', ' ').replace('.', ',')
 
+    # Répartition des coûts par opération
+    costs_by_operation = (
+        entretien_data.values('operation')
+        .annotate(total_cost=Sum('cost'))
+        .order_by('-total_cost')
+    )
+    if not immat:
+        # Si aucune immatriculation n'est sélectionnée, on prend les 5 premières opérations
+        costs_by_operation = costs_by_operation[:5]
+
+    operation_fig = px.pie(
+        names=[item['operation'] for item in costs_by_operation],
+        values=[item['total_cost'] for item in costs_by_operation],
+        
+    )
+    operation_chart = mark_safe(operation_fig.to_html(full_html=False))
+
+# barplot des coûts totaux par immatriculation
+
+    # barplot des coûts totaux par immatriculation (top 8) avec axes inversés
+    bar_fig = px.bar(
+        x=[item['total_cost'] for item in entretien_data.values('immat').annotate(total_cost=Sum('cost')).order_by('-total_cost')[:8]],  # Axe X : coûts totaux
+        y=[item['immat'] for item in entretien_data.values('immat').annotate(total_cost=Sum('cost')).order_by('-total_cost')[:8]],  # Axe Y : immatriculations
+        orientation='h'  # Orientation horizontale
+    )
+    # Supprimer les titres des axes
+    bar_fig.update_xaxes(title=None)  # Supprimer le titre de l'axe X
+    bar_fig.update_yaxes(title=None)  # Supprimer le titre de l'axe Y
+
+
+    bar_fig.update_yaxes(categoryorder='total ascending')
+
+
+    operation_histogram = mark_safe(bar_fig.to_html(full_html=False))
+
+
+
     return render(request, 'dashboard/data.html', {
         'entretien_data': entretien_data,
         'distinct_immat': distinct_immat,  # Passer les immatriculations distinctes au template
         'distinct_operations': distinct_operation,  # Passer les opérations distinctes au template
         'total_cost': formatted_total_cost,  # Passer le coût total au template
+        'operation_chart': operation_chart,  # Graphique des opérations
+        'operation_histogram': operation_histogram,  # Graphique des opérations
     })
